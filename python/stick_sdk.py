acf942abd15dd7cbb268fbac6316f2620f60b346
code560 <smartphone.560@gmail.com>
2018-07-29 21:17:03 +0900

    mod

--- python/stick_sdk.py
+++ python/stick_sdk.py
@@ -34,7 +34,7 @@ class Stick():
         get_accel = self.lib.get_accel
         get_accel.argtypes = [POINTER(c_ushort)]
         array = POINTER(c_ushort)
-        val = array() * 3
+        val = array()
         get_accel(byref(val))
         logger.d('get accel = ({},{},{})'.format(val[0], val[1], val[2]))
         return val
