26a34ec4ab566ceaff84241995c8a37f7fe64a7b
code560 <smartphone.560@gmail.com>
2018-08-10 21:31:29 +0900

    mod

--- python/sdk_test.py
+++ python/sdk_test.py
@@ -10,18 +10,26 @@ if __name__ == '__main__':
     s.init_sdk()
     s.stop_demo()
 
-    # t = STICK
-    # t.init_sdk()
-    # t.stop_led_demo()
+    h = 32
+    s.write(0, [0x000000 for _ in range(h)])
+    s.write(1, [0xff0000 for _ in range(h)])
+    s.write(2, [0x00ff00 for _ in range(h)])
+    s.write(3, [0x0000ff for _ in range(h)])
+    s.write(4, [0xffff00 for _ in range(h)])
+    s.write(5, [0xff00ff for _ in range(h)])
+    s.write(6, [0x00ffff for _ in range(h)])
+    s.write(7, [0xffffff for _ in range(h)])
 
     while True:
         try:
             accel = s.accel()
             gyro = s.gyro()
-            # accel = t.get_accel()
-            # gyro = t.get_gyro()
             logger.d('get accel = ({},{},{})'.format(accel[0], accel[1], accel[2]))
             logger.d('get gyro = ({},{},{})'.format(gyro[0], gyro[1], gyro[2]))
+
+            line = (accel[1] + 0x8000) * 7 / 0x10000
+            s.show(line)
+            logger.d('show line={}'.format(line))
 
             time.sleep(0.2)
 
