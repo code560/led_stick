500ebf1f87d54c1179ea9235f4bb47c14a355587
code560 <smartphone.560@gmail.com>
2018-08-11 15:48:27 +0900

    mod

--- python/show_pic.py
+++ python/show_pic.py
@@ -15,17 +15,18 @@ def write(stick, im, lines):
     px = np.array(im)
     px = px // Stick.LED_FACTOR * Stick.LED_FACTOR
     for x in range(im.width):
-        # pattern = [0] * (len_ * 3)
-        # for y in range(im.height):
-        #     try:
-        #         r, g, b = get_led_rgb(px[y, x])
-        #         pattern[y * 3] = r
-        #         pattern[y * 3 + 1] = g
-        #         pattern[y * 3 + 2] = b
-        #     except Exception:
-        #         logger.e('x={}, y={}'.format(x, y, r))
-        #         raise
-        pattern = np.ravel(px[x])
+        pattern = [0] * (len_ * 3)
+        for y in range(im.height):
+            try:
+                # r, g, b = get_led_rgb(px[y, x])
+                r, g, b = int(px[x])
+                pattern[y * 3] = r
+                pattern[y * 3 + 1] = g
+                pattern[y * 3 + 2] = b
+            except Exception:
+                logger.e('x={}, y={}'.format(x, y, r))
+                raise
+        # pattern = np.ravel(px[x])
         stick.write(x, pattern)
 
 
